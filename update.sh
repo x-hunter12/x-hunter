a5cebad566450fd319320d42dca77d2a2160046d
x-hunter12 <154734962+x-hunter12@users.noreply.github.com>
2023-12-24 19:33:11 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+clear
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+res1() {
+#wget -O menu.zip "https://vpn-store.sgpremium.biz.id:81/sc"
+unzip menu.zip
+chmod +x menu/*
+mv menu/* /usr/local/sbin
+rm -fr menu
+rm -fr menu.zip
+rm -fr update.sh
+
+#wget -O alt.zip "https://vpn-store.sgpremium.biz.id:81/alt"
+unzip alt.zip
+chmod +x all-t/*
+mv all-t/* /usr/bin
+rm -fr all-t
+rm -fr alt.zip
+
+rm -fr /usr/sbin/xdxl/style
+mkdir -p /usr/sbin/xdxl/style
+#wget -O style.zip "https://vpn-store.sgpremium.biz.id:81/style.zip"
+unzip style.zip
+chmod +x style/*
+mv style/* /usr/sbin/xdxl/style
+rm -fr style
+rm -fr style.zip
+}
+netfilter-persistent
+clear
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m             UPDATE SCRIPT              \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e "  \033[1;91m update script service\033[1;37m"
+fun_bar 'res1'
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -n 1 -s -r -p "Press [ Enter ] to back on menu"
+menu
